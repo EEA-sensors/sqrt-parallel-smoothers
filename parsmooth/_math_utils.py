c13b794b43f6679a6b153a589c4bf8882c509373
adriencorenflos <adrien.corenflos@gmail.com>
2021-09-06 17:30:54 +0300

    Temporary fix for chol update

--- parsmooth/_math_utils.py
+++ parsmooth/_math_utils.py
@@ -1,11 +1,11 @@
 import jax
+import jax.numpy as jnp
 import jax.scipy.linalg as jlinalg
-import tensorflow_probability.substrates.jax as tfp
 
 
 def cholesky_update_many(chol_init, update_vectors, multiplier):
     def body(chol, update_vector):
-        res = tfp.math.cholesky_update(chol, update_vector, multiplier=multiplier)
+        res = _cholesky_update(chol, update_vector, multiplier=multiplier)
         return res, None
 
     final_chol, _ = jax.lax.scan(body, chol_init, update_vectors)
@@ -15,3 +15,61 @@ def cholesky_update_many(chol_init, update_vectors, multiplier):
 def tria(A):
     tria_A = jlinalg.qr(A.T, mode='economic')[1].T
     return tria_A
+
+
+def _set_diagonal(x, y):
+    N, _ = x.shape
+    i, j = jnp.diag_indices(N)
+    return x.at[i, j].set(y)
+
+
+def _set_triu(x, val):
+    N, _ = x.shape
+    i = jnp.triu_indices(N, 1)
+    return x.at[i].set(val)
+
+
+def _cholesky_update(chol, update_vector, multiplier=1.):
+    # FIXME: This is fixed by the PR https://github.com/tensorflow/probability/pull/1423/files.
+    #        Will be deleted once it is fixed by Tensorflow Probability
+
+    chol_diag = jnp.diag(chol)
+
+    # The algorithm in [1] is implemented as a double for loop. We can treat
+    # the inner loop in Algorithm 3.1 as a vector operation, and thus the
+    # whole algorithm as a single for loop, and hence can use a `tf.scan`
+    # on it.
+
+    # We use for accumulation omega and b as defined in Algorithm 3.1, since
+    # these are updated per iteration.
+
+    def scan_body(carry, inp):
+        _, _, omega, b = carry
+        index, diagonal_member, col = inp
+        omega_at_index = omega[..., index]
+
+        # Line 4
+        new_diagonal_member = jnp.sqrt(jnp.square(diagonal_member) + multiplier / b * jnp.square(omega_at_index))
+        # `scaling_factor` is the same as `gamma` on Line 5.
+        scaling_factor = (jnp.square(diagonal_member) * b + multiplier * jnp.square(omega_at_index))
+
+        # The following updates are the same as the for loop in lines 6-8.
+        omega = omega - (omega_at_index / diagonal_member)[..., None] * col
+        new_col = new_diagonal_member[..., None] * (
+                col / diagonal_member[..., None] +
+                (multiplier * omega_at_index / scaling_factor)[..., None] * omega)
+        b = b + multiplier * jnp.square(omega_at_index / diagonal_member)
+        return (new_diagonal_member, new_col, omega, b), (new_diagonal_member, new_col, omega, b)
+
+    # We will scan over the columns.
+    chol = chol.T
+
+    _, (new_diag, new_chol, _, _) = jax.lax.scan(scan_body,
+                                                 (0., jnp.zeros_like(chol[0]), update_vector, 1.),
+                                                 (jnp.arange(0, chol.shape[0]), chol_diag, chol),
+                                                 )
+
+    new_chol = new_chol.T
+    new_chol = _set_diagonal(new_chol, new_diag)
+    new_chol = _set_triu(new_chol, 0.)
+    return new_chol
--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -1,10 +1,52 @@
+import jax
+import numpy as np
 import pytest
 
+from parsmooth._math_utils import _cholesky_update, cholesky_update_many
 
-@pytest.mark.parametrize("multiplier", [1., -1.])
-@pytest.mark.parametrize("seed", [0, 1, 2, 3])
-def test_cholesky_update_many(multiplier, seed):
-    pass
+
+@pytest.fixture(scope="session")
+def config():
+    jax.config.update("jax_enable_x64", True)
+
+
+@pytest.mark.parametrize("multiplier", [1., -0.1])
+@pytest.mark.parametrize("seed", [0, 42, 666])
+@pytest.mark.parametrize("dim_x", [2, 3, 10, 11])
+def test_cholesky_update(multiplier, seed, dim_x):
+    np.random.seed(seed)
+    cholQ = np.random.rand(dim_x, dim_x)
+    cholQ[np.triu_indices(dim_x, 1)] = 0.
+    v = np.random.randn(dim_x)  # needs to be small enough...
+
+    expected = cholQ @ cholQ.T + multiplier * v[:, None] @ v[None, :]
+    eigs_expected = np.linalg.eigvals(expected)
+    if min(eigs_expected) <= 1e-6:
+        pytest.skip("random vectors do not result in a positive definite matrix.")
+
+    cholRes = _cholesky_update(cholQ, v, multiplier)
+
+    np.testing.assert_allclose(cholRes @ cholRes.T, expected, rtol=1e-4)
+
+
+@pytest.mark.parametrize("multiplier", [1., -0.1])
+@pytest.mark.parametrize("seed", [0, 1, 2, 42, 666])
+@pytest.mark.parametrize("dim_x", [2, 3, 10, 11])
+def test_cholesky_update_many(multiplier, seed, dim_x):
+    np.random.seed(seed)
+    B = 3
+    cholQ = np.random.rand(dim_x, dim_x)
+    cholQ[np.triu_indices(dim_x, 1)] = 0.
+    v = np.random.rand(B, dim_x)  # needs to be small enough...
+
+    expected = cholQ @ cholQ.T + multiplier * sum([v[k, :, None] @ v[k, None, :] for k in range(B)])
+    eigs_expected = np.linalg.eigvals(expected)
+    if min(eigs_expected) <= 1e-6:
+        pytest.skip("random vectors do not result in a positive definite matrix.")
+
+    cholRes = cholesky_update_many(cholQ, v, multiplier)
+
+    np.testing.assert_allclose(cholRes @ cholRes.T, expected, rtol=1e-4)
 
 
 @pytest.mark.parametrize("seed", [0, 1, 2, 3])
