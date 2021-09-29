9e82fc91f98b33c2d86b4466c2fe3d77874cbaff
AdrienCorenflos <adrien.corenflos@gmail.com>
2021-09-29 15:03:37 +0300

    Pathwise sample of smoothing solutions (#11)
    * Implement a pathwise sampler for the sequential smoothers
    
    * Fixed a trivial bug in incremental standard cov
    
    * Fixed the sqrt sampler
    
    * Implement a pathwise sampler for the sequential smoothers
    
    * Fixed a trivial bug in incremental standard cov
    
    * Fixed the sqrt sampler
    
    * parallel sampler implemented
    
    * Simplified as suggested by the awesome amazing Fatemeh

---
+++ parsmooth/_pathwise_sampler.py
@@ -0,0 +1,126 @@
+from functools import partial
+from typing import Callable
+
+import jax
+import jax.numpy as jnp
+import jax.scipy.linalg as jlag
+
+from parsmooth._base import MVNStandard, MVNSqrt, FunctionalModel
+from parsmooth._utils import tria, none_or_shift, none_or_concat
+
+
+def _par_sampling(key: jnp.ndarray,
+                  n_samples: int,
+                  transition_model: FunctionalModel,
+                  filter_trajectory: MVNSqrt or MVNStandard,
+                  linearization_method: Callable,
+                  nominal_trajectory: MVNStandard or MVNSqrt):
+    gains, incs, last_state_sample = _sampling_common(key, n_samples, transition_model, filter_trajectory,
+                                                      linearization_method, nominal_trajectory)
+
+    @jax.vmap
+    def operator(elem1, elem2):
+        G1, e1 = elem1
+        G2, e2 = elem2
+
+        G = G2 @ G1
+        e = jnp.einsum("ij,...j->...i", G2, e1) + e2
+        return G, e
+
+    Gs = none_or_concat(gains, jnp.zeros_like(gains[0]), -1)
+    es = none_or_concat(incs, last_state_sample, -1)
+
+    _, sampled_states = jax.lax.associative_scan(operator,
+                                                 [Gs, es],
+                                                 reverse=True)
+
+    return sampled_states
+
+
+def _seq_sampling(key: jnp.ndarray,
+                  n_samples: int,
+                  transition_model: FunctionalModel,
+                  filter_trajectory: MVNSqrt or MVNStandard,
+                  linearization_method: Callable,
+                  nominal_trajectory: MVNStandard or MVNSqrt):
+    gains, incs, last_state_sample = _sampling_common(key, n_samples, transition_model, filter_trajectory,
+                                                      linearization_method, nominal_trajectory)
+
+    def body(prev_xs, inputs):
+        gain, inc = inputs
+        xs = jnp.einsum("ij,...j->...i", gain, prev_xs) + inc
+        return xs, xs
+
+    _, sampled_states = jax.lax.scan(body,
+                                     last_state_sample,
+                                     [gains, incs],
+                                     reverse=True)
+
+    sampled_states = none_or_concat(sampled_states, last_state_sample, -1)
+    return sampled_states
+
+
+def _sampling_common(key: jnp.ndarray,
+                     n_samples: int,
+                     transition_model: FunctionalModel,
+                     filter_trajectory: MVNSqrt or MVNStandard,
+                     linearization_method: Callable,
+                     nominal_trajectory: MVNStandard or MVNSqrt):
+    last_state = jax.tree_map(lambda z: z[-1], filter_trajectory)
+    filter_trajectory = none_or_shift(filter_trajectory, -1)
+    F_x, cov_or_chol, b = jax.vmap(linearization_method, in_axes=[None, 0])(transition_model,
+                                                                            none_or_shift(nominal_trajectory, -1))
+    T = F_x.shape[0]
+    eps = jax.random.normal(key, (T + 1, n_samples) + b.shape[1:])
+
+    if isinstance(filter_trajectory, MVNSqrt):
+        gain, inc = jax.vmap(_sqrt_gain_and_inc)(F_x, cov_or_chol, b, filter_trajectory, eps[1:])
+    else:
+        gain, inc = jax.vmap(_standard_gain_and_inc)(F_x, cov_or_chol, b, filter_trajectory, eps[1:])
+
+    last_state_sample = _make_mvn(last_state, eps[0])
+
+    return gain, inc, last_state_sample
+
+
+@partial(jax.vmap, in_axes=[None, 0])
+def _make_mvn(x: MVNStandard or MVNSqrt, eps: jnp.ndarray):
+    m, cov_or_chol = x
+    if isinstance(x, MVNStandard):
+        L = jnp.linalg.cholesky(cov_or_chol)
+    elif isinstance(x, MVNSqrt):
+        L = cov_or_chol
+    else:
+        raise ValueError(f"`x` must be a MVNStandard or a MVNSqrt. {type(x)} was passed")
+    return m + L @ eps
+
+
+def _standard_gain_and_inc(F, Q, b, xf, eps):
+    mf, Pf = xf
+
+    S = F @ Pf @ F.T + Q
+    gain = Pf @ jlag.solve(S, F, sym_pos=True).T
+
+    inc_Sig = Pf - gain @ S @ gain.T
+    inc_m = mf - gain @ (F @ mf + b)
+
+    inc = _make_mvn(MVNStandard(inc_m, inc_Sig), eps)
+    return gain, inc
+
+
+def _sqrt_gain_and_inc(F, cholQ, b, xf, eps):
+    mf, cholPf = xf
+
+    nx = F.shape[0]
+    Phi = jnp.block([[F @ cholPf, cholQ],
+                     [cholPf, jnp.zeros_like(F)]])
+    tria_Phi = tria(Phi)
+    Phi11 = tria_Phi[:nx, :nx]
+    Phi21 = tria_Phi[nx:, :nx]
+    inc_L = tria_Phi[nx:, nx:]
+
+    gain = jlag.solve_triangular(Phi11, Phi21.T, trans=True, lower=True).T
+    inc_m = mf - gain @ (F @ mf + b)
+
+    inc = _make_mvn(MVNSqrt(inc_m, inc_L), eps)
+    return gain, inc
---
+++ tests/test_sampler.py
@@ -0,0 +1,64 @@
+from functools import partial
+
+import jax
+import jax.numpy as jnp
+import numpy as np
+import pytest
+
+from parsmooth._base import FunctionalModel, MVNStandard, MVNSqrt
+from parsmooth.linearization import cubature, extended
+from parsmooth.methods import sampling
+from parsmooth.sequential._filtering import filtering
+from parsmooth.sequential._smoothing import smoothing
+from tests._lgssm import get_data, transition_function as lgssm_f, observation_function as lgssm_h
+from tests._test_utils import get_system
+
+LIST_LINEARIZATIONS = [cubature, extended]
+
+
+@pytest.fixture(scope="session", autouse=True)
+def config():
+    jax.config.update("jax_enable_x64", True)
+    jax.config.update('jax_disable_jit', False)
+    jax.config.update("jax_debug_nans", False)
+
+
+@pytest.mark.parametrize("dim_x", [1, 3])
+@pytest.mark.parametrize("dim_y", [2, 3])
+@pytest.mark.parametrize("seed", [0])
+@pytest.mark.parametrize("linearization", LIST_LINEARIZATIONS)
+@pytest.mark.parametrize("jax_seed", [123])
+@pytest.mark.parametrize("parallel", [False, True])
+def test_samples_marginals(dim_x, dim_y, seed, linearization, jax_seed, parallel):
+    np.random.seed(seed)
+    key = jax.random.PRNGKey(jax_seed)
+
+    T = 10
+    N = 10_000
+
+    x0, chol_x0, F, Q, cholQ, b, _ = get_system(dim_x, dim_x)
+    _, _, H, R, cholR, c, _ = get_system(dim_x, dim_y)
+
+    _, ys = get_data(x0.mean, F, H, R, Q, b, c, T)
+    sqrt_transition_model = FunctionalModel(partial(lgssm_f, A=F), MVNSqrt(b, cholQ))
+
+    transition_model = FunctionalModel(partial(lgssm_f, A=F), MVNStandard(b, Q))
+    observation_model = FunctionalModel(partial(lgssm_h, H=H), MVNStandard(c, R))
+
+    for method in LIST_LINEARIZATIONS:
+        filtered_states = filtering(ys, x0, transition_model, observation_model, method)
+        smoothed_states = smoothing(transition_model, filtered_states, method)
+        samples = sampling(key, N, transition_model, filtered_states, method, smoothed_states, parallel=parallel)
+
+        sqrt_filtered_states = MVNSqrt(filtered_states.mean, jax.vmap(jnp.linalg.cholesky)(filtered_states.cov))
+        sqrt_smoothed_states = MVNSqrt(smoothed_states.mean, jax.vmap(jnp.linalg.cholesky)(smoothed_states.cov))
+        sqrt_samples = sampling(key, N, sqrt_transition_model, sqrt_filtered_states, method, sqrt_smoothed_states,
+                                parallel=parallel)
+
+        np.testing.assert_allclose(samples.mean(1), smoothed_states.mean, rtol=1e-2, atol=1e-2)
+        np.testing.assert_allclose(samples.var(1), np.diagonal(smoothed_states.cov, axis1=1, axis2=2),
+                                   rtol=1e-1, atol=5e-2)
+        np.testing.assert_allclose(sqrt_samples.mean(1), sqrt_smoothed_states.mean,
+                                   rtol=1e-2, atol=1e-2)
+        np.testing.assert_allclose(sqrt_samples.var(1), np.diagonal(smoothed_states.cov, axis1=1, axis2=2),
+                                   rtol=1e-1, atol=5e-2)
