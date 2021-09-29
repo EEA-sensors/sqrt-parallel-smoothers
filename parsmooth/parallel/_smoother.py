ed1e76e67c6f95265202604662a701cc2f8eca9e
AdrienCorenflos <adrien.corenflos@gmail.com>
2021-09-29 13:15:27 +0300

    Parallel smoother (#12)
    * Improved the tests, changed some formulations in square root of update.
    
    * first commit
    
    * Just changed name of methods
    
    * Test was not targeting the smoother. This commit corrects it.
    
    * Don't debug for nans
    
    * Test one step parallel operators.
    
    * Some formatting
    
    * standard and sqrt version of parallel Kalman filter.
    
    * tests for standard and sqrt associative params.
    
    * Test one step parallel operators.
    
    * Some formatting
    
    * standard and sqrt version of parallel Kalman filter.
    
    * tests for standard and sqrt associative params.
    
    * Merge remote-tracking branch 'origin/parallel-filter' into main
    
    # Conflicts:
    #	parsmooth/sequential/_filter.py
    #	parsmooth/sequential/_smoother.py
    #	tests/test_sequential_filter.py
    #	tests/test_sequential_smoother.py
    
    * Force float64 for JAX
    
    * This is working mostly. Now the only non-working bit is n_x < n_y. I am really not sure how to make this bit work though
    
    * Concatenate syntax is more clear
    
    * Less time steps for the test.
    
    * Add default for the nominal trajectory.
    
    * Smoothing implemented with tests.
    
    * Reduce tolerance requirement a bit.
    
    * Fix default traj
    
    Co-authored-by: Fatemeh Yaghoobi <fatemeh.yaghoobi@aalto.fi>

--- parsmooth/parallel/_smoother.py
+++ parsmooth/parallel/_smoother.py
@@ -1,11 +1,82 @@
-def smoother(filter_trajectory, transition_function, observation_function, linearization_method, sqrt,
-             nominal_trajectory=None):
-    pass
+from typing import Callable, Optional
+
+import jax
+import jax.numpy as jnp
+import jax.scipy.linalg as jlinalg
+
+from parsmooth._base import MVNStandard, FunctionalModel, MVNSqrt, are_inputs_compatible
+from parsmooth._utils import none_or_concat, tria
+from parsmooth.parallel._operators import sqrt_smoothing_operator, \
+    standard_smoothing_operator
 
 
-def _standard_smoother():
-    pass
+def smoothing(transition_model: FunctionalModel, filter_trajectory: MVNSqrt or MVNStandard,
+              linearization_method: Callable, nominal_trajectory: Optional[MVNStandard or MVNSqrt] = None):
+    if nominal_trajectory is not None:
+        are_inputs_compatible(filter_trajectory, nominal_trajectory)
+
+    else:
+        ms, chol_or_covs = filter_trajectory
+        T, nx = ms.shape
+        nominal_mean = jnp.zeros_like(ms)
+        nominal_cov_or_chol = jnp.repeat(jnp.eye(nx)[None, ...], T, 0)
+
+        # this is kind of a hack but I've seen worse.
+        nominal_trajectory = type(filter_trajectory)(nominal_mean, nominal_cov_or_chol)
+
+    if isinstance(filter_trajectory, MVNSqrt):
+        associative_params = _associative_params(linearization_method, transition_model,
+                                                 nominal_trajectory, filter_trajectory, True)
+        smoothed_means, _, smoothed_chols = jax.lax.associative_scan(jax.vmap(sqrt_smoothing_operator),
+                                                                     associative_params, reverse=True)
+        res = jax.vmap(MVNSqrt)(smoothed_means, smoothed_chols)
+
+    else:
+        associative_params = _associative_params(linearization_method, transition_model,
+                                                 nominal_trajectory, filter_trajectory, False)
+        smoothed_means, _, smoothed_covs = jax.lax.associative_scan(jax.vmap(standard_smoothing_operator),
+                                                                    associative_params, reverse=True)
+        res = jax.vmap(MVNStandard)(smoothed_means, smoothed_covs)
+
+    return res
 
 
-def _sqrt_smoother():
-    pass
+def _associative_params(linearization_method, transition_model,
+                        nominal_trajectory, filtering_trajectory, sqrt):
+    ms, Ps = filtering_trajectory
+    nominal_trajectory = jax.tree_map(lambda z: z[:-1], nominal_trajectory)
+    if sqrt:
+        vmapped_fn = jax.vmap(_sqrt_associative_params, in_axes=[None, None, 0, 0, 0])
+    else:
+        vmapped_fn = jax.vmap(_standard_associative_params, in_axes=[None, None, 0, 0, 0])
+    gs, Es, Ls = vmapped_fn(linearization_method, transition_model, nominal_trajectory, ms[:-1], Ps[:-1])
+    g_T, E_T, L_T = ms[-1], jnp.zeros_like(Ps[-1]), Ps[-1]
+    return none_or_concat((gs, Es, Ls), (g_T, E_T, L_T), -1)
+
+
+def _standard_associative_params(linearization_method, transition_model, n_k_1, m, P):
+    F, Q, b = linearization_method(transition_model, n_k_1)
+    Pp = F @ P @ F.T + Q
+
+    E = jlinalg.solve(Pp, F @ P, sym_pos=True).T
+
+    g = m - E @ (F @ m + b)
+    L = P - E @ Pp @ E.T
+
+    return g, E, L
+
+
+def _sqrt_associative_params(linearization_method, transition_model, n_k_1, m, chol_P):
+    F, chol_Q, b = linearization_method(transition_model, n_k_1)
+    nx = chol_Q.shape[0]
+
+    Phi = jnp.block([[F @ chol_P, chol_Q],
+                     [chol_P, jnp.zeros_like(chol_Q)]])
+    Tria_Phi = tria(Phi)
+    Phi11 = Tria_Phi[:nx, :nx]
+    Phi21 = Tria_Phi[nx:, :nx]
+    D = Tria_Phi[nx:, nx:]
+
+    E = jlinalg.solve(Phi11.T, Phi21.T).T
+    g = m - E @ (F @ m + b)
+    return g, E, D
---
+++ tests/test_parallel_smoother.py
@@ -0,0 +1,99 @@
+from functools import partial
+
+import jax
+import numpy as np
+import pytest
+
+from parsmooth._base import MVNStandard, FunctionalModel, MVNSqrt
+from parsmooth.linearization import cubature, extended
+from parsmooth.parallel._smoother import _standard_associative_params, _sqrt_associative_params, \
+    smoothing as par_smoothing
+from parsmooth.sequential._filter import filtering as seq_filtering
+from parsmooth.sequential._smoother import smoothing as seq_smoothing
+from tests._lgssm import transition_function as lgssm_f, observation_function as lgssm_h, get_data
+from tests._test_utils import get_system
+
+LIST_LINEARIZATIONS = [cubature, extended]
+
+
+@pytest.fixture(scope="session", autouse=True)
+def config():
+    jax.config.update("jax_enable_x64", True)
+    jax.config.update('jax_disable_jit', False)
+    jax.config.update("jax_debug_nans", False)
+
+
+@pytest.mark.parametrize("dim_x", [1, 2, 3])
+@pytest.mark.parametrize("dim_y", [1, 2, 3])
+@pytest.mark.parametrize("seed", [0, 42])
+@pytest.mark.parametrize("linearization_method", LIST_LINEARIZATIONS)
+def test_params_standard_vs_sqrt(dim_x, dim_y, seed, linearization_method):
+    np.random.seed(seed)
+
+    x0, chol_x0, F, Q, cholQ, b, _ = get_system(dim_x, dim_x)
+
+    _, _, H, R, cholR, c, _ = get_system(dim_x, dim_y)
+    m_nominal = np.random.randn(dim_x)
+    P_nominal = np.eye(dim_x, dim_x)
+    cholP_nominal = P_nominal
+    x_nominal_sqrt = MVNSqrt(m_nominal, cholP_nominal)
+    x_nominal_std = MVNStandard(m_nominal, P_nominal)
+    y = np.random.randn()
+
+    sqrt_transition_model = FunctionalModel(partial(lgssm_f, A=F), MVNSqrt(b, cholQ))
+
+    transition_model = FunctionalModel(partial(lgssm_f, A=F), MVNStandard(b, Q))
+
+    g_std, E_std, L = _standard_associative_params(linearization_method, transition_model, x_nominal_std, x0.mean,
+                                                   x0.cov)
+    g_sqrt, E_sqrt, D = _sqrt_associative_params(linearization_method, sqrt_transition_model, x_nominal_sqrt,
+                                                 chol_x0.mean, chol_x0.chol)
+
+    np.testing.assert_allclose(g_std, g_sqrt)
+    np.testing.assert_allclose(E_std, E_sqrt)
+    np.testing.assert_allclose(L, D @ D.T)
+
+
+@pytest.mark.parametrize("dim_x", [1, 2, 3])
+@pytest.mark.parametrize("dim_y", [1, 2, 3])
+@pytest.mark.parametrize("seed", [0, 42])
+@pytest.mark.parametrize("linearization_method", LIST_LINEARIZATIONS)
+def test_vs_sequential_smoother(dim_x, dim_y, seed, linearization_method):
+    np.random.seed(seed)
+    T = 15
+
+    x0, chol_x0, F, Q, cholQ, b, _ = get_system(dim_x, dim_x)
+
+    _, _, H, R, cholR, c, _ = get_system(dim_x, dim_y)
+
+    m_nominal = np.random.randn(T + 1, dim_x)
+    P_nominal = np.repeat(np.eye(dim_x, dim_x)[None, ...], T + 1, axis=0)
+    cholP_nominal = P_nominal
+    x_nominal_sqrt = MVNSqrt(m_nominal, cholP_nominal)
+    x_nominal = MVNStandard(m_nominal, P_nominal)
+
+    true_states, observations = get_data(x0.mean, F, H, R, Q, b, c, T)
+
+    sqrt_transition_model = FunctionalModel(partial(lgssm_f, A=F), MVNSqrt(b, cholQ))
+    sqrt_observation_model = FunctionalModel(partial(lgssm_h, H=H), MVNSqrt(c, cholR))
+
+    transition_model = FunctionalModel(partial(lgssm_f, A=F), MVNStandard(b, Q))
+    observation_model = FunctionalModel(partial(lgssm_h, H=H), MVNStandard(c, R))
+
+    seq_filter_res = seq_filtering(observations, x0, transition_model, observation_model, linearization_method,
+                                   x_nominal)
+    seq_smoother_res = seq_smoothing(transition_model, seq_filter_res, linearization_method)
+    par_smoother_res = par_smoothing(transition_model, seq_filter_res, linearization_method)
+
+    seq_sqrt_filter_res = seq_filtering(observations, chol_x0, sqrt_transition_model, sqrt_observation_model,
+                                        linearization_method,
+                                        x_nominal_sqrt)
+    seq_sqrt_smoother_res = seq_smoothing(sqrt_transition_model, seq_sqrt_filter_res, linearization_method)
+    par_sqrt_smoother_res = par_smoothing(sqrt_transition_model, seq_sqrt_filter_res, linearization_method)
+
+    np.testing.assert_array_almost_equal(seq_smoother_res.mean, par_smoother_res.mean, decimal=4)
+    np.testing.assert_array_almost_equal(seq_smoother_res.cov, par_smoother_res.cov, decimal=4)
+    np.testing.assert_array_almost_equal(seq_sqrt_smoother_res.mean, par_sqrt_smoother_res.mean, decimal=4)
+    np.testing.assert_array_almost_equal(
+        seq_sqrt_smoother_res.chol @ np.transpose(seq_sqrt_smoother_res.chol, [0, 2, 1]),
+        seq_smoother_res.cov, decimal=4)
