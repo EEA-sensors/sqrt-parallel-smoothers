aedf6e186c81344300fea028c7cd58b432a212f2
Adrien Corenflos <adrien.corenflos@gmail.com>
2024-05-22 15:52:15 +0100

    Bump version to 1.0.0

--- parsmooth/__init__.py
+++ parsmooth/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.0"
+__version__ = "1.0.0"
 
 from ._base import MVNSqrt, MVNStandard, FunctionalModel, ConditionalMomentsModel
 from .methods import filtering, smoothing, iterated_smoothing, filter_smoother, sampling
